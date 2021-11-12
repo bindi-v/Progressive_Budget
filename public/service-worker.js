54767b05c89cb04b6034efffb1b5c6ea4b198597
bindi-v <bindi.vaghela@gmail.com>
2021-11-11 21:57:07 -0500

    rename css file so that its link work

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,7 @@ const FILES_TO_CACHE = [
     "/",
     "/indexdDb.js",
     "/index.js",
-    "/style.css",
+    "/styles.css",
     "/manifest.json",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
