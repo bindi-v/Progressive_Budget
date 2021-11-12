88873263b9e38ab477c7a0c7b877ec37f31ac597
bindi-v <bindi.vaghela@gmail.com>
2021-11-11 19:25:40 -0500

    added link in files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,18 @@
+const CACHE_NAME = "static-cache-v1";
+  const DATA_CACHE_NAME = "data-cache-v1";
+
 const FILES_TO_CACHE = [
     "/",
+    "/bundle.js",
     "/index.js",
     "/style.css",
     "/manifest.json",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
   ];
   
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
+  
 
   // install
 self.addEventListener("install", function(evt) {
